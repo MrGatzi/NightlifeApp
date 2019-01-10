1260ee33c95f53af1ada6ab2449df29bde7237c4
juanisi <S1510238026@students.fh-hagenberg.at>
2019-01-10 17:30:51 +0100

    API Call

--- app/src/main/java/com/example/nightlife/nightlife/MainActivity.java
+++ app/src/main/java/com/example/nightlife/nightlife/MainActivity.java
@@ -19,6 +19,21 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -45,6 +60,30 @@ public class MainActivity extends AppCompatActivity {
         /*
         * DUMMY OBJECT START -----------------------------------------------------------------------
         */
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://nightlifeapi.projekte.fh-hagenberg.at/laravel/public/api/location/0/0";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("myState", response);
+                        // Display the first 500 characters of the response string.
+                        //mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("myState", "error");
+
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
         List<Location> venues = new ArrayList<Location>();
         List<Location> events = new ArrayList<Location>();
         List<List<Location>> locations = new ArrayList<List<Location>>();
